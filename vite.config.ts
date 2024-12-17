e972d6bbd66e5e05ed07aa506d58641750ae587b
Faig Eyvazli <faig.eyvazli@gmail.com>
2024-12-17 13:42:07 +0400

    chore: remove redundant path resolve

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
   resolve: {
     alias: {
-      '@app': path.resolve(__dirname, 'src'),
+      '@app': '/src',
     },
   },
   plugins: [react()],
